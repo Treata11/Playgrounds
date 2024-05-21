96180199aaf0005906ab3c2d768a3b9dfa6922e5
Treata11 <treata11@yahoo.com>
2024-05-21 12:46:33 +0330

    Initial Commit

---
+++ CBass Tests/CBass_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBass_TestsApp.swift
+//  CBass Tests
+//
+//  Created by Treata Norouzi on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CBass_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
